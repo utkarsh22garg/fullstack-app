1c09205e8be8223f56d4e95b48c5e92d3796734b
Utkarsh Garg <utkarsh22garg@gmail.com>
2022-04-24 14:06:27 +0530

    finally setup eslint with prettier

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,21 @@
-import 'dotenv/config'; // Setup env vars in process.env
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import "dotenv/config"; // Setup env vars in process.env
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
-const app = express()
+const app = express();
 const port = parseInt(process.env.SERVER_PORT, 10) | 4000;
 
 // Setup express middlewares
-app.use(helmet());  // Security Headers
-app.use(cors());  // CORS protection
-app.use(morgan('dev')); // Request Logger
+app.use(helmet()); // Security Headers
+app.use(cors()); // CORS protection
+app.use(morgan("dev")); // Request Logger
 
-app.get('/test', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
